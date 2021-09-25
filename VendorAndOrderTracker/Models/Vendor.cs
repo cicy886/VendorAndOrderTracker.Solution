6314b7ba1c2679278ff651c8f1979c4d6bf96c3f
Sisi Vieira <cicy886@gmail.com>
2021-09-25 15:54:05 -0700

    create Vendor, order list; create Name, Id properties

--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -4,6 +4,9 @@ namespace ProjectName.Models
 {
   public class Vendor
   {
-
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+    public int Id { get; }
+    public List<Order> Items { get; set; }
   }
 }
