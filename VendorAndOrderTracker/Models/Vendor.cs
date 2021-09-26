2de590744d25a7bbe3b5e7fc5741d972c53f3bf3
Sisi Vieira <cicy886@gmail.com>
2021-09-25 17:04:01 -0700

    define ClearAll() method

--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -2,19 +2,27 @@ using System.Collections.Generic;
 
 namespace VendorAndOrderTracker.Models
 {
-  public class Vendor
-  {
-    private static List<Vendor> _instances = new List<Vendor> {};
-    public string Name { get; set; }
-    public int Id { get; }
-    public List<Order> Orders { get; set; }
-
-    public Vendor(string vendorName)
+    public class Vendor
     {
-      Name = vendorName;
-      _instances.Add(this);
-      Id = _instances.Count;
-      Orders = new List<Order>{};
+        private static List<Vendor> _instances = new List<Vendor> { };
+
+        public string Name { get; set; }
+
+        public int Id { get; }
+
+        public List<Order> Orders { get; set; }
+
+        public Vendor(string vendorName)
+        {
+            Name = vendorName;
+            _instances.Add(this);
+            Id = _instances.Count;
+            Orders = new List<Order> { };
+        }
+
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
     }
-  }
 }
