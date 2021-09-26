de2ab4f51afd9d7806e3be297b3e82d0bc0bd805
Sisi Vieira <cicy886@gmail.com>
2021-09-25 19:10:04 -0700

    create a Create() route that creates new Orders within a given Vendor

--- VendorAndOrderTracker/Controllers/VendorsController.cs
+++ VendorAndOrderTracker/Controllers/VendorsController.cs
@@ -50,5 +50,18 @@ namespace VendorAndOrderTracker.Controllers
             model.Add("vendor", foundVendor);
             return View("Show", model);
         }
+
+        [HttpPost("/vendors/{vendorId}/orders")]
+        public ActionResult Create(int vendorId, string orderDescription)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Vendor foundVendor = Vendor.Find(vendorId);
+            Order newOrder = new Order(orderDescription);
+            foundVendor.AddOrder (newOrder);
+            List<Order> vendorOrders = foundVendor.Orders;
+            model.Add("orders", vendorOrders);
+            model.Add("vendor", foundVendor);
+            return View("Show", model);
+        }
     }
 }
