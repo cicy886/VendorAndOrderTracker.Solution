d12ab19bebd990ee049ff02a6cd2dbdde016d450
Sisi Vieira <cicy886@gmail.com>
2021-09-25 17:18:17 -0700

    add Find() method. all tests passed

--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -29,5 +29,10 @@ namespace VendorAndOrderTracker.Models
         {
             return _instances;
         }
+
+        public static Vendor Find(int searchId)
+        {
+            return _instances[searchId - 1];
+        }
     }
 }
