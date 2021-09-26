081a1d736171223b6552e780df29ef35c26e363b
Sisi Vieira <cicy886@gmail.com>
2021-09-25 17:11:29 -0700

    add a test for testing functions for retrieving Vedor objects to display

--- VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -47,5 +47,23 @@ namespace VendorAndOrderTracker.Tests
             //Assert
             Assert.AreEqual(1, result);
         }
+
+        [TestMethod]
+        public void GetAll_ReturnsAllVendorObjects_VendorList()
+        {
+            //Arrange
+            string name01 = "Suzi's Cafe";
+            string name02 = "Starbucks";
+            Vendor newVendor1 = new Vendor(name01);
+            Vendor newVendor2 = new Vendor(name02);
+            List<Vendor> newList =
+                new List<Vendor> { newVendor1, newVendor2 };
+
+            //Act
+            List<Vendor> result = Vendor.GetAll();
+
+            //Assert
+            CollectionAssert.AreEqual (newList, result);
+        }
     }
 }
